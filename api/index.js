cb4d0f7bcd07b2d101ede81e8f485c272b1f11c3
Vaishnav2607 <vaishnavnarigiri267@gmail.com>
2024-02-21 00:18:08 +0530

    Create SignUp api route

--- api/index.js
+++ api/index.js
@@ -2,9 +2,12 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+
 app.listen(2140, () => {
   console.log("Server is Running on 2140!!!");
 });
@@ -16,5 +19,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-
+// Getting routes from controllers and routes
+// controllers contain response functions for controlling the routing
   app.use('/api/user', userRouter);
+  app.use('/api/auth', authRouter);
