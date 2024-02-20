c2958592cde445ef93ff62197715d1763e8af674
Vaishnav2607 <vaishnavnarigiri267@gmail.com>
2024-02-20 23:11:29 +0530

    Connect to Database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
-import express from 'express'
-const app = express()
-app.listen(2140, () =>{
-    console.log("Server is Running on 2140!!!");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.listen(2140, () => {
+  console.log("Server is Running on 2140!!!");
 });
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
