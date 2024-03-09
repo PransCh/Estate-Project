f4cafd51d769db02cc6d602629b91c702981c5d7
Vaishnav2607 <vaishnavnarigiri267@gmail.com>
2024-03-09 17:00:42 +0530

    Complete header search form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import { useSelector } from "react-redux";
 export default function Header() {
+  const navigate = useNavigate();
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState('');
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('searchTerm', searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(() =>{
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermfromUrl = urlParams.get('searchTerm');
+    if(searchTermfromUrl){
+      setSearchTerm(searchTermfromUrl);
+    }
+  },[location.search])
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -13,13 +29,17 @@ export default function Header() {
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={((e) => setSearchTerm(e.target.value))}
           />
+          <button >
           <FaSearch className="ext-slate-600" />
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link to="/">
