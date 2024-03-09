9dd8756710150f0e03e0fab6508c3e1c785016fb
Vaishnav2607 <vaishnavnarigiri267@gmail.com>
2024-03-10 00:28:28 +0530

    Add pre Installation

--- api/index.js
+++ api/index.js
@@ -5,15 +5,9 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config();
 
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
-
-app.listen(2140, () => {
-  console.log("Server is Running on 2140!!!");
-});
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
@@ -22,11 +16,27 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(2140, () => {
+  console.log("Server is Running on 2140!!!");
+});
+
 // Getting routes from controllers and routes
 // controllers contain response functions for controlling the routing
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
