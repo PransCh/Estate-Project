3012142421d31be6c876f67719beda530078ca1e
Vaishnav2607 <vaishnavnarigiri267@gmail.com>
2024-03-09 15:58:59 +0530

    Add contact landlord to listing page and complete listing page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -267,7 +267,7 @@ export default function Profile() {
               </Link>
               <Link
                 className="text-slate-700 font-semibold hover:underline truncate flex-1"
-                to={`listing/${listing._id}`}
+                to={`/listing/${listing._id}`}
               >
                 <p className="">{listing.name}</p>
               </Link>
