4dbe5ee472acc63993157e4738b30b9bbe763e95
Vaishnav2607 <vaishnavnarigiri267@gmail.com>
2024-03-10 08:53:52 +0530

    Fixed a typo in ListingItem page

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -12,6 +12,7 @@ export default function Contact({ listing }) {
       try {
         const res = await fetch(`/api/user/${listing.userRef}`);
         const data = await res.json();
+        // console.log("Landlord:", data); 
         setLandlord(data);
       } catch (error) {
         console.log(error);
